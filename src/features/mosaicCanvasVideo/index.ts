7f22471d4d7f508230d31e62e9eac5902b09bc11
Patrick S. Young <patrick@bigeyeball.co>
2020-10-15 13:09:37 -0700

    add directory index to make imports more readable

---
+++ src/features/mosaicCanvasVideo/index.ts
@@ -0,0 +1,8 @@
+export { MosaicSelector } from 'features/mosaicCanvasVideo/MosaicSelector';
+export { MosaicTiles } from 'features/mosaicCanvasVideo/MosaicTiles';
+export { MosaicTile } from 'features/mosaicCanvasVideo/MosaicTile';
+export { setMosaicVideo } from 'features/mosaicCanvasVideo/mosaicSlice';
+export { setMosaicCanvas } from 'features/mosaicCanvasVideo/mosaicSlice';
+export { setNumTiles } from 'features/mosaicCanvasVideo/mosaicSlice';
+export type { MosaicState } from 'features/mosaicCanvasVideo/mosaicSlice';
+export type { NumTiles } from 'features/mosaicCanvasVideo/mosaicSlice';
