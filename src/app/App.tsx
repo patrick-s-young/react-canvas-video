f01bf36dd3f92fa9710ae2d4cdc5cb6366ad14c3
Patrick S. Young <patrick@bigeyeball.co>
2020-10-21 23:50:44 -0700

    incorporate video upload mock

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,70 +1,33 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setVideoData } from 'features/userVideo/videoSlice';
-import { MosaicTiles, setMosaicCanvas } from 'features/mosaicCanvasVideo';
-import { loadVideoMetadata, preloadVideo} from 'utils';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { MosaicTiles } from 'features/mosaicCanvasVideo';
+import { VideoUpload } from 'features/userVideo/VideoUpload';
 import type { VideoState } from 'features/userVideo/videoSlice';
 import type { RootState } from 'app/rootReducer';
-import type { VideoMetadata } from 'utils';
 import 'app/app.css';
 
 const App: React.FC = () => {
-	const [error, setError] = useState<string | null>(null);
-	const [videoSrc, setVideoSrc] = useState<string | null>(null);
-	const dispatch = useDispatch();
-	const { src, width } = useSelector<RootState, VideoState>(
-		(state) => state.video
-	);
-  const { canvasWidth } = useSelector(
-		( state: RootState ) => state.mosaic
-	);
-
-	const onSetVideoData = (videoMetadata: VideoMetadata) =>
-		dispatch(setVideoData({
-			src: videoSrc,
-			duration: videoMetadata.duration,
-			width: videoMetadata.videoWidth,
-			height: videoMetadata.videoHeight
-		})
-	);
-
-	const onSetMosaicCanvas = (videoWidth: number) => {
-		const canvasWidth = videoWidth > window.innerWidth ? window.innerWidth : videoWidth;
-		dispatch(setMosaicCanvas(canvasWidth));
+	// conditional render <VideoUpload> or <MosaicTiles>
+	const [videoReady, setVideoReady] = useState<boolean>(false);
+	// set app screen width relative to video width
+	const { src, duration, width, height } = useSelector<RootState, VideoState>((state) => state.video as VideoState);
+	const appScreenWidth: Partial<number> = width > window.innerWidth ? window.innerWidth : width;
+	// callback from <VideoUpload> when user-selected video is ready
+	const onVideoSelectedCallback = () => {
+		setVideoReady(true);
 	}
-
-	useEffect(() => {
-		// placeholder for user file upload functionality
-		preloadVideo('video/swing_480x480.mp4')
-		.then(setVideoSrc)
-		.catch(setError);
-	}, []);
-
-	useEffect(() => {
-		// retrieve video metadata
-		if (videoSrc !== null) {
-    	loadVideoMetadata(videoSrc, 2000)
-      	.then(onSetVideoData)
-				.catch(setError);
-		}
-  }, [videoSrc]);
-
-	useEffect(() => {
-		if (width !== null) {
-			onSetMosaicCanvas(width);
-		}
-	}, [width]);
-
+	// <VideoUpload> is a mock for user-initiated video uploads
 	return (
-			<div className='app-container'>
-      	{src !== null &&
-					<div style={{width: `${canvasWidth}px`}}>
-						<MosaicTiles />
-					</div>
+			<div className='app-container' style={{width: `${appScreenWidth}px`}}>
+				{!videoReady
+					? <VideoUpload onVideoSelectedCallback={onVideoSelectedCallback}/>
+					: <MosaicTiles
+							src={src}
+							duration={duration}
+							width={width}
+							height={height}/>
 				}
-				{src === null && <div>LOADING...</div>}
-      	{error !== null && <div>{error}</div>}
 			</div>
 	);
 }
