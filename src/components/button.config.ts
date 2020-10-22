2d31ca66ecf2b0115e3bbf18aca2f6d96a3adaec
Patrick S. Young <patrick@bigeyeball.co>
2020-10-21 23:43:42 -0700

    reduce file size of image-based selector buttons

--- src/components/button.config.ts
+++ src/components/button.config.ts
@@ -1,18 +1,22 @@
 import { 
   NumTiles          // buttons in <MosiacSelector>
 } from 'features/mosaicCanvasVideo/mosaicSlice';
+import { CSSProperties } from 'react';
 
 // union NumTiles with future state value types that will be set using <Button> component
 export type StateValue =  NumTiles; 
 
 export interface ButtonConfig {
   stateValue: StateValue
-  imagePath: { default: string, hilite: string }
+  imagePath: string
   altText: string
 }
 
 export interface ButtonProps extends ButtonConfig {
   onClickCallback: (newStateValue: StateValue) => void
-  className: string
+  className: { 
+    default: string,
+    hilite: string
+  }
   isEnabled: boolean
 }
